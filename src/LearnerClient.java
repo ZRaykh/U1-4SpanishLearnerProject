e44b0e4a657e249e6ca4de44984fe31dcc71b63e
BT_4N2_04 <zraykh2784@bths.edu>
2022-11-14 12:31:38 -0500

    Created days of the week learner

--- src/LearnerClient.java
+++ src/LearnerClient.java
@@ -1,54 +1,61 @@
 import java.util.Scanner;
 public class LearnerClient {
+    //program outputs
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
 
         //greeting program and setting up username
         System.out.println("Hello user! Welcome to Spanish Learner, a program that is aimed to help you study spanish" +
-                "at a basic skill level! Please enter a username:");
+                " at a basic skill level! Please enter a username:");
+        Scanner s = new Scanner(System.in);
         String user = s.nextLine();
+
+       //checking if the user wants english to spanish practice or spanish to english. Only checks for B because that is the only setting that sets Spanish to English.
         System.out.println("Well then " + user + " do you want to practice converting \"A\" English words to Spanish or \"B\" Spanish words to English?");
-        String practice = s.nextLine().toUpperCase();
-        if (!(practice.equals("A") || practice.equals("B"))) {
-            System.out.println("User input incorrect, resorting to default settings.");
-            practice = "A";
-        }
-        System.out.println("Last question: Do you want skill leveling active? If yes please enter \"Y\", enter anything else for no.");
         String input = s.nextLine().toUpperCase();
-        if (input.equals("Y"))
+        boolean practice = true;
+        if (input.equals("B"))
         {
-            System.out.println("Please enter a number 1 through 3 for the skill leve. 1 is easiest difficulty, 3 is hardest.");
-            int skill = s.nextInt();
-            if (skill == 1)
-            {
-                System.out.println("Easy skill level selected.");
-            }
-            if (skill == 2)
-            {
-                System.out.println("Medium skill level selected.");
-            }
-            if (skill == 3)
-            {
-                System.out.println("Hard skill level selected.");
-            }
-            else
-            {
-                System.out.println("No valid input detected, default easy option entered.");
-                skill = 1;
-            }
-            SpanishLearner spanishPractice = new SpanishLearner(user, practice, skill);
+            practice = false;
         }
-        else
-        {
-            SpanishLearner spanishPractice = new SpanishLearner(user, practice);
-        }
+        else if (!(input.equals("A")))
+            System.out.println("User input incorrect, resorting to default settings.");
+
+        //creates the object "sp" which stands for Spanish Practice.
+        SpanishLearner sp = new SpanishLearner(user, practice);
         System.out.println("All set up! Ready to go!");
+
+        input = "";
+        //spanish practice starts here
         while (!input.equals("END"))
         {
             System.out.println("Current practice modes include: \"A\" days of the week, \"B\" seasons, \"C\" months \"D\" numbers. " +
                     "Or enter \"end\" to exit, or \"edit\" to change your practice mode.\nPlease select your choice:");
             input = s.nextLine().toUpperCase();
-        }
 
+            //days of the week practice
+            if (input.equals("A"))
+            {
+                System.out.println("Days of the week selected. Please use \" ' \" after a letter to indicate accent marks. Example: í would be i'");
+                if (sp.isEnglishToSpanish())
+                {
+                    System.out.println("What is Monday in spanish");
+                    String monday = s.nextLine().toLowerCase();
+                    System.out.println("What is Tuesday in spanish");
+                    String tuesday = s.nextLine().toLowerCase();
+                    System.out.println("What is Wednesday in spanish");
+                    String wednesday = s.nextLine().toLowerCase();
+                    System.out.println("What is Thursday in spanish");
+                    String thursday = s.nextLine().toLowerCase();
+                    System.out.println("What is Friday in spanish");
+                    String friday = s.nextLine().toLowerCase();
+                    System.out.println("What is Saturday in spanish");
+                    String saturday = s.nextLine().toLowerCase();
+                    System.out.println("What is Sunday in spanish");
+                    String sunday = s.nextLine().toLowerCase();
+                    System.out.println(sp.daysOfWeek(monday, tuesday, wednesday, thursday, friday, saturday, sunday));
+                }
+            }
+        }
+        //while loop ends here
     }
 }
--- src/SpanishLearner.java
+++ src/SpanishLearner.java
@@ -1,33 +1,182 @@
+
 public class SpanishLearner {
     //the username that will be reported
     private String user;
     //Used to determine
-    private String practice;
+    private boolean isEnglishToSpanish;
     //Used for level scaling to track the user's progress
     private int level;
 
-    public SpanishLearner (String username, String practiceForm, int userLevel)
+   //class that tracks the user level
+    public SpanishLearner (String username, boolean spanishToEnglish, int userLevel)
     {
         user = username;
-        practice = practiceForm;
+        isEnglishToSpanish = spanishToEnglish;
         level = userLevel;
     }
-    public SpanishLearner (String username, String practiceForm)
+
+    //user level is disabled
+    public SpanishLearner (String username, boolean spanishToEnglish)
     {
         user = username;
-        practice = practiceForm;
+        isEnglishToSpanish = spanishToEnglish;
         level = -1;
     }
+
+    //swaps the practice modes around
     public void updatePractice()
     {
-        if (practice.equals("StE"))
-            practice = "EtS";
-        else
-            practice = "StE";
-    }
-    public int getLevel()
-    {
-        return level;
+        isEnglishToSpanish = !isEnglishToSpanish;
     }
 
+    //returns the level of the user
+    public boolean isEnglishToSpanish()
+    {
+        return isEnglishToSpanish;
+    }
+
+    //answer sheet for days of the week, each boolean value stands for if the answer is right or wrong.
+    public String daysOfWeek(String monday, String tuesday, String wednesday, String thursday, String friday, String saturday, String sunday)
+    {
+        String answerSheet;
+        int count = 0;
+        if (isEnglishToSpanish)
+        {
+            if (monday.equals("lunes"))
+            {
+                answerSheet = "Monday: Correct!";
+                count ++;
+            }
+            else
+                answerSheet = "Monday: Incorrect! The correct answer is lunes!";
+
+            if (tuesday.equals("martes"))
+            {
+                answerSheet += "\nTuesday: Correct!";
+                count ++;
+            }
+            else
+                answerSheet += "\nTuesday: incorrect! The correct answer is martes!";
+
+            if (wednesday.equals("mie'rcoles"))
+            {
+                answerSheet += "\nWednesday: Correct!";
+                count ++;
+            }
+            else
+            {
+                answerSheet += "\nWednesday: incorrect! The correct answer is miércoles! Written as: \"mie'rcoles\" on your end";
+            }
+
+            if (thursday.equals("jueves"))
+            {
+                answerSheet += "\nThursday: Correct!";
+                count ++;
+            }
+            else
+            {
+                answerSheet += "\nThursday: incorrect! The correct answer is jueves!";
+            }
+
+            if (friday.equals("viernes"))
+            {
+                answerSheet += "\nFriday: Correct!";
+                count ++;
+            }
+            else
+            {
+                answerSheet += "\nFriday: incorrect! The correct answer is viernes!";
+            }
+
+            if (saturday.equals("sa'bado"))
+            {
+                answerSheet += "\nSaturday: Correct!";
+                count ++;
+            }
+            else
+            {
+                answerSheet += "\nSaturday: incorrect! The correct answer is sábado!";
+            }
+
+            if (sunday.equals("domingo"))
+            {
+                answerSheet += "\nSunday: Correct!";
+                count ++;
+            }
+            else
+            {
+                answerSheet += "\nSunday: incorrect! The correct answer is domingo!";
+            }
+        }
+        else
+        {
+            if (monday.equals("monday"))
+            {
+                answerSheet = "Lunes: Correct!";
+                count ++;
+            }
+            else
+                answerSheet = "Lunes: Incorrect! Lunes is Monday in English!";
+
+            if (tuesday.equals("tuesday"))
+            {
+                answerSheet += "\nMartes: Correct!";
+                count ++;
+            }
+            else
+                answerSheet += "\nMartes: incorrect! Martes is Tuesday in English!";
+
+            if (wednesday.equals("wednesday"))
+            {
+                answerSheet += "\nMiércoles: Correct!";
+                count ++;
+            }
+            else
+            {
+                answerSheet += "\nMiércoles: incorrect! Miércoles is Wednesday in English!";
+            }
+
+            if (thursday.equals("thursday"))
+            {
+                answerSheet += "\nJueves: Correct!";
+                count ++;
+            }
+            else
+            {
+                answerSheet += "\nJueves: incorrect! Jueves is Thursday in English!";
+            }
+
+            if (friday.equals("friday"))
+            {
+                answerSheet += "\nViernes: Correct!";
+                count ++;
+            }
+            else
+            {
+                answerSheet += "\nViernes: incorrect! Viernes is Friday in English!";
+            }
+
+            if (saturday.equals("saturday"))
+            {
+                answerSheet += "\nSábado: Correct!";
+                count ++;
+            }
+            else
+            {
+                answerSheet += "\nSábado: incorrect! Sábado is saturday in English!";
+            }
+
+            if (sunday.equals("sunday"))
+            {
+                answerSheet += "\nDomingo: Correct!";
+                count ++;
+            }
+            else
+            {
+                answerSheet += "\nDomingo: incorrect! Domingo is Sunday in English!!";
+            }
+        }
+        answerSheet += "\n You got" + count / 7.0 + " correct!";
+        return answerSheet;
+    }
 }
